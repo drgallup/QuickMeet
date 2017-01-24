1fc8e92807e5bd837322565294e4822c4a2de1e0
nathphan <nathphan510@gmail.com>
2017-01-23 17:19:08 -0800

    Updated CalendarGenerator.js to draw grid

--- CalendarGenerator.js
+++ CalendarGenerator.js
@@ -2,12 +2,44 @@
 //  CalendarGenerator.js
 //  generates the calendar for quick calendar
 //
-//  Version 0.1 - Dylan, 11am, 1/23/17
+//  Version 0.2 - Nathan, 5pm, 1/23/17
 //  - Loads canvas from index.html
 //  - Makes it 2d
+//  - Draws grid
 // --------------------------------------------------------------------
 
 // get the canvas we are working on
 // it's global so it can be called in functions
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+
+// Gets canvas width and height
+var width = myCanvas.width,
+    height = myCanvas.height;
+
+//Predefined number of rows and columns
+var columns = 7,
+	rows = 24;
+
+//Gets size of each cell
+var tileWidth = width / columns,
+	tileHeight = height / rows;
+
+var context = myCanvas.getContext("2d");
+
+//Draws grid
+function drawGrid(){
+	for (var x = 0; x<=width; x+=tileWidth){
+		context.moveTo(x,0);
+		context.lineTo(x,height);
+	}
+
+	for (var y = 0; y<=height; y+=tileHeight){
+		context.moveTo(0,y);
+		context.lineTo(width,y);
+	}
+	context.strokeStyle = "black";
+	context.stroke();
+}
+
+drawGrid();
