0a90df647d8b1830e19cb3a459ecf64246d88609
imancini <imancini@ucsc.edu>
2017-01-30 10:08:11 -0800

    GenerateBox.js
    This will be a grid of boxes within the canvas calendar that will highlight themselves/fill/appear when clicked on  (eventually when click and dragged on)

---
+++ GenerateBox.js
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------
+// //  GenerateBox.js
+// //  
+// //  create grid of boxes inside of the canvas
+// //  get x,y coordinates from on-click function 
+// //  check if the mouse coordinates are within the coordinates of any of the boxes
+// //  if within coordinate of a box, fill box with a color/turn view of box on/ whatever
+// //  
+// //  after you have this working, add a function that checks the delta x and delta y from the drag clicks
+// //  highlight multiple boxes in the grid corresponding to the relative delta x and delta y values returned from click and drag
+// //
+// //  Version 0.1 - Isaac, 1/30/17
+// //  -Add event listener on Canvas for click
+// // --------------------------------------------------------------------
