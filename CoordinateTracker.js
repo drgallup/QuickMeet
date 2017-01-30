485134ead1db523ac4fdf96eb827dd79c92ec4ca
drgmonkey <dylangallup@gmail.com>
2017-01-30 10:31:01 -0800

    Add files via upload

--- CoordinateTracker.js
+++ CoordinateTracker.js
@@ -18,18 +18,29 @@ var can = document.getElementById('myCanvas'),
     context = can.getContext('2d'),
     element = [];
 
-can.onclick = function(event) {
+can.onmousedown = function(event) {
   // saves first click, changes state
   if(evenclick){
     x1 = event.pageX - canLeft;
     y1 = event.pageY - canTop;
     evenclick = false;
   }
+}
+
+can.onmouseup = function(e){
   // saves second click, changes state
-  else if(!evenclick){
-    x2 = event.pageX - canLeft;
-    y2 = event.pageY - canTop;
+  if(!evenclick){
+    x2 = e.pageX - canLeft;
+    y2 = e.pageY - canTop;
     evenclick = true;
-    //alert("Width: " + (x2-x1) + " Length: " + (y2-y1));
+    alert("Width: " + (x2-x1) + " Length: " + (y2-y1) + " x1: " + x1 + " y1: " + y1 + " x2: " + x2 + " y2: " + y2);
   }
+  
+  
+}
+
+function findLocation (x1, y1, x2, y2){
+  // figures out which hours on the calendar have been selected
+  
+  
 }
