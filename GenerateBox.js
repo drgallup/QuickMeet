ec741b5d1921d58911bcb88215ab417bf2be6ab7
imancini <imancini@ucsc.edu>
2017-02-01 10:18:31 -0800

    Update GenerateBox.js

--- GenerateBox.js
+++ GenerateBox.js
@@ -13,33 +13,35 @@
 // //  -Add event listener on Canvas for click
 // // --------------------------------------------------------------------
 
-function changeColor(evt){
-  var clickedOn = evt.target;
-  // for HTML
-  clickedOn.style.backgroundColor = '#f00';
-
-  // for SVG
-  clickedOn.setAttribute('fill','red');
-}
-mySquare.addEventListener('click',changeColor,false);
-
-
 var lastClicked;
-var grid = clickableGrid(700,100,function(el,row,col,i){
+var grid = clickableGrid(12,7,function(el,row,col,i){
+    console.log("You clicked on element:",el);
+    console.log("You clicked on row:",row);
+    console.log("You clicked on col:",col);
+    console.log("You clicked on item #:",i);
 
+    el.className='clicked';
+    if (lastClicked) lastClicked.className='';
+    lastClicked = el;
+});
+
+document.body.appendChild(grid);
+     
 function clickableGrid( rows, cols, callback ){
-  var i=0;
-  var grid = document.createElement('table');
-  grid.className = 'grid';
-  for (var r=0;r<rows;++r){
-    var tr = grid.appendChild(document.createElement('tr'));
-    for (var c=0;c<cols;++c){
-      var cell = tr.appendChild(document.createElement('td'));
-      cell.innerHTML = ++i;
-      cell.addEventListener('click',(function(el,r,c,i){
-        return function(){ callback(el,r,c,i); }
-       })(cell,r,c,i),false);
+    var i=0;
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var r=0;r<rows;++r){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var cell = tr.appendChild(document.createElement('td'));
+            //cell.innerHTML = ++i;
+            cell.addEventListener('click',(function(el,r,c,i){
+                return function(){
+                    callback(el,r,c,i);
+                }
+            })(cell,r,c,i),false);
+        }
     }
-  }
-  return grid;
+    return grid;
 }
