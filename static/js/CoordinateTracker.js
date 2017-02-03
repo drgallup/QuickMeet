ea872a292865e6f1bc6b34b452f4a1427e6ff578
drgmonkey <dylangallup@gmail.com>
2017-02-03 14:15:04 -0800

    Added Day and Hour detection

--- static/js/CoordinateTracker.js
+++ static/js/CoordinateTracker.js
@@ -2,17 +2,18 @@
 // //  CoordinateTracker.js
 // //  Track the x,y coordinate when mouse click event happen on canvas
 // //
-// //  Version 0.5 - Nathan, 10am, 2/3/17
-// //  -Add event listener on Canvas for on mouse click, on mouse movement, 
-// //   and on mouse release
-// //  -Creates live rendering box when you click and drag
+// //  Version 0.1 - Kevin, 6, 1/24/17
+// //  -Add event listener on Canvas for click
 // // --------------------------------------------------------------------
+
+var evenclick = true;
+var x1;
+var y1;
+var x2;
+var y2;
 var day = [ [100, 200], [200, 300],
             [300, 400], [400, 500],
             [500, 600], [600, 700], [700, 800] ];
-
-var canvas, startX, endX, startY, endY;
-var mouseIsDown = false;
 
 var can = document.getElementById('myCanvas'),
     canLeft = can.offsetLeft,
@@ -20,72 +21,35 @@ var can = document.getElementById('myCanvas'),
     context = can.getContext('2d'),
     element = [];
 
-can.addEventListener('mousedown', mouseDown, false);
-can.addEventListener('mousemove', mouseMove, false);
-can.addEventListener('mouseup', mouseUp, false);
+can.onmousedown = function(event) {
+  // saves first click, changes state
+  if(evenclick){
+    x1 = event.pageX - canLeft;
+    y1 = event.pageY - canTop;
+    evenclick = false;
+  }
+}
 
-function mouseUp(eve) {
-    if (mouseIsDown != false) {
-        mouseIsDown = false;
-        var pos = getMousePos(canvas, eve);
-        endX = pos.x;
-        endY = pos.y;
-        drawSquare(); 
-    }
-    ctx.clearRect(0,0,c.width,c.height);
-    drawGrid();
+can.onmouseup = function(e){
+  // saves second click, changes state
+  if(!evenclick){
+    x2 = e.pageX - canLeft;
+    y2 = e.pageY - canTop;
+    evenclick = true;
+    //alert("Width: " + (x2-x1) + " Length: " + (y2-y1) + " x1: " + x1 + " y1: " + y1 + " x2: " + x2 + " y2: " + y2);
     findLocation();
-}
-
-function mouseDown(eve) {
-    mouseIsDown = true;
-    var pos = getMousePos(canvas, eve);
-    startX = endX = pos.x;
-    startY = endY = pos.y;
-    drawSquare(); 
-}
-
-function mouseMove(eve) {
-    if (mouseIsDown !== false) {
-        var pos = getMousePos(canvas, eve);
-        endX = pos.x;
-        endY = pos.y;
-        drawSquare();
-    }
-}
-
-function drawSquare() {
-    // creating a square
-    var w = endX - startX;
-    var h = endY - startY;
-    var offsetX = (w < 0) ? w : 0;
-    var offsetY = (h < 0) ? h : 0;
-    var width = Math.abs(w);
-    var height = Math.abs(h);
-               
-    ctx.beginPath();
-    ctx.fillStyle = "rgba(128,0,0,1)";
-    ctx.fillRect(startX + offsetX, startY + offsetY, width, height);
-    ctx.lineWidth = 1;
-}
-
-function getMousePos(canvas, evt) {
-    var rect = can.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
+  }
+  
+  
 }
 
 function findLocation (){
   // figures out which hours on the calendar have been selected
-  var selected;
-  var dayTemp;
-  var hourTemp;
   for (var i = 0; i<day.length; i++){
-    if( day[i][0] < startX && endX < day[i][1] ){
-      dayTemp.push(i);
+    if( day[i][0] < x1 && x1 < day[i][1] ){
+      alert("Day " + i + " selected!");
     }
-    alert(dayTemp);
   }
+  
+  
 }
