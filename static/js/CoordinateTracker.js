09e6770b44f8c23ebebdd9f052eb48da5ae111c5
Nathan Phan <nathphan510@gmail.com>
2017-03-06 10:27:03 -0800

    Add files via upload

--- static/js/CoordinateTracker.js
+++ static/js/CoordinateTracker.js
@@ -93,7 +93,12 @@ function mouseUp(eve) {
     }
     ctx.clearRect(0,0,c.width,c.height);
     drawGrid();
-    findLocation();
+    if(deletion==false){
+      findLocation();
+    }
+    if(deletion==true){
+      findDeletion();
+    }
     drawBox(btimeStart, btimeEnd, bdayStart, bdayEnd);
      
     
@@ -203,6 +208,52 @@ function getMousePos(canvas, evt) {
         x: evt.clientX - rect.left,
         y: evt.clientY - rect.top
     };
+}
+
+function findDeletion(){
+  var dayTemp = [];
+  var hourTemp = [];    
+  for (var i = 0; i<day.length-1; i++){
+    if( day[i] < startX && startX < day[i+1] ){
+      dayTemp.push(i);
+    }else if( startX < day[i] && day[i+1] < endX ){
+      dayTemp.push(i);
+    }else if( day[i] < endX && endX < day[i+1] ){
+      dayTemp.push(i);
+    }
+  }
+
+  for (var i = 0; i<hour.length-1; i++){
+    if( hour[i] < startY && startY < hour[i+1] ){
+      hourTemp.push(i);
+    }else if( startY < hour[i] && hour[i+1] < endY ){
+      hourTemp.push(i);
+    }else if( hour[i] < endY && endY < hour[i+1] ){
+      hourTemp.push(i);
+    }
+  }
+  var timeStart = timeCalc(hourTemp[0]);
+  var timeEnd = timeCalc(hourTemp[hourTemp.length-1]);
+
+  var dayStart = dayTemp[0];
+  var dayEnd = dayTemp[dayTemp.length-1];
+  var counter = 0;
+
+  for (a = btimeStart.length-1; a>=0; a--) {
+        if((btimeStart[a]<=timeStart) && (timeEnd<=btimeEnd[a]) && (bdayStart[a]<=dayStart) && (dayEnd<=bdayEnd[a])){
+            alert(a + " true");
+            btimeStart.splice(a,1);
+            btimeEnd.splice(a,1);
+            bdayStart.splice(a,1);
+            bdayEnd.splice(a,1);
+            counter = counter + 1;
+            ctx.clearRect(0,0,c.width,c.height);
+            drawGrid();
+            drawBox(btimeStart, btimeEnd, bdayStart, bdayEnd); 
+            console.log("Deleted");
+            break;
+        } 
+    }
 }
 
 function findLocation (){
